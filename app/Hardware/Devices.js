d47ffc6fec00a431508df76bb79674f4b41b1e11
Alexey Kashintsev <ak@altsoft.biz>
2015-12-14 17:48:52 +0400

    Work version

--- app/Hardware/Devices.js
+++ app/Hardware/Devices.js
@@ -5,8 +5,8 @@
  * @public 
  * @module Devices
  */
-define(['orm', 'logger', 'Trigger'], function (Orm
-, Logger, Trigger, ModuleName) {
+define(['orm', 'logger', 'Trigger', 'TemperatureSensor'], function (Orm
+, Logger, Trigger, TemperatureSensor, ModuleName) {
     return function () {
         var self = this, model = Orm.loadModel(ModuleName);
         var devs = {};
