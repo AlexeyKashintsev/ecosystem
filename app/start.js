28b92430a034776fdc5b0c466205db61bef0fdfe
Toma <tktenshi@mail.ru>
2016-04-22 15:44:02 +0300

    del start.js

--- app/start.js
+++ app/start.js
@@ -6,15 +6,15 @@ require(['facade', 'logger'], function (F, Logger) {
     var global = this;
     F.cacheBust(true);
     F.export(global);
-    require('testUi', function(testUi){
-        var m = new testUi();
+    require('DevicesView', function(DevicesView){
+        var m = new DevicesView();
         m.show();
     }, function(e){
         Logger.severe(e);
         if(global.document){
             var messageParagraph = global.document.createElement('p');
             global.document.body.appendChild(messageParagraph);
-            messageParagraph.innerHTML = 'An error occured while require(\'testUi\'). Error: ' + e;
+            messageParagraph.innerHTML = 'An error occured while require(\'DevicesView\'). Error: ' + e;
             messageParagraph.style.margin = '10px';
             messageParagraph.style.fontFamily = 'Arial';
             messageParagraph.style.fontSize = '14pt';
